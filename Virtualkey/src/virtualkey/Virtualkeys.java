fc5e204d86eebcc2d865a788d11a9540155afa27
Vimal29-r <106429259+Vimal29-r@users.noreply.github.com>
2022-07-30 20:03:15 +0530

    Add files via upload

---
+++ Virtualkey/src/virtualkey/Virtualkeys.java
@@ -0,0 +1,38 @@
+package virtualkey;
+import java.util.*;
+public class Virtualkeys {
+	public static void main(String[] args) {
+		
+		Virtualkey screen = new Virtualkey();
+		screen.welcomeScreen();
+		Boolean quit = true;
+		int choice = 0;
+		while(quit) {
+			try {
+				Scanner userChoice = new Scanner(System.in);
+				System.out.println("\nEnter your choice: ");
+				choice = userChoice.nextInt();
+			}
+			catch(InputMismatchException e){
+				System.out.println("Invalid Input..Try again!!");
+				continue;
+			}
+			
+			switch(choice) {
+				case 1 :
+					screen.showFiles();
+					break;
+				case 2:
+					screen.moreOptions();
+					break;
+				case 3:
+					System.out.println("\nProgram closed!!");
+					quit = false;
+					break;
+				default :
+					System.out.println("Invalid input..Try again!!");
+				}
+		}
+	}
+
+}
